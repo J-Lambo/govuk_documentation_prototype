39d08f3d191531fdfa759cb5010535bfcd301a15
Tim Paul <contact@timpaul.co.uk>
2016-09-10 08:16:37 +0100

    Add version 1 prototype

--- source/javascripts/all.js
+++
@@ -1,7 +0,0 @@
-//= require ./lib/_energize
-//= require ./app/_lang
-//= require ./app/_search
-//= require ./app/_toc
-//= require ./app/_js
-//= require ./app/_selectionbuttons
-//= require ./app/_application

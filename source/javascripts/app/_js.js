a94788457e83ec9de6939c340c610b0fe7c5ee74
Tim Paul <contact@timpaul.co.uk>
2016-09-12 13:15:49 +0100

    Add horizontal nav

--- source/javascripts/app/_js.js
+++ source/javascripts/app/_js.js
@@ -18,11 +18,13 @@ $(document).ready(function() {
 
 // Hide document title in site header if you're at the top of the page
 // This is to avoid duplication with the H1 of the documentation
+/*
 var $document = $(document);
 $('#proposition-name').addClass('visually-hidden');
 $document.scroll(function() {
 	$('#proposition-name').toggleClass('visually-hidden', $document.scrollTop() <= 60);
 });
+*/
 
 
 $("#input-search").clearable();
